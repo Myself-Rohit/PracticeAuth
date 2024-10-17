ca860972af6c9494f87900a7c219fb768e15b925
rohit <rohitnegi263139@gmail.com>
2024-10-17 13:26:50 +0530

    fixing proxy error

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,19 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
-		port: 5173,
 		proxy: {
 			"/api": {
-				target: "http://localhost",
-				changeOrigin: true,
+				target: "http://localhost:8000",
 				secure: false,
-				ws: false,
-			},
-			"/PluginsAPI": {
-				target: "https://localhost:55434/",
-				changeOrigin: true,
-				secure: false,
-				ws: false,
 			},
 		},
 	},
